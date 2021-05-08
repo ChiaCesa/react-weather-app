3531c621724c966fe3dd0720feb42829cc943e6d
Chiara <kia.cesa@gmail.com>
2021-05-08 19:47:09 +0200

    re-doing structure+css

--- src/App.js
+++ src/App.js
@@ -6,13 +6,16 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="container">
      
-        <h1>
-          
-        </h1>
-     <Weather />
-      </header>
+        <h1></h1>
+          <Weather />
+    <footer>
+      This project was coded by Chiara Cesana and is
+      <a href="https://github.com/ChiaCesa/react-weather-app" target="_blank"> open-sourced on GitHub</a>
+    </footer>
+    </div>
+    
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -3,100 +3,47 @@ import  "./Weather.css";
 //import axios from "axios";
 
 
-export default function Weather() {
- 
-   let weatherData = {
-      city: "New York",
-      date: "Tuesday 10:00",
-      description: "Cloudy",
-      imgUrl: "https://ssl.gstatic.com/onebox/weather/64/cloudy.png",
-      humidity: 50,
-      wind: 4,
-      temperature: 15
-    };
-    
-  
-    return (
-        <div className="container">
-      <div className="Weather">
-        <form className="form-floating" />
+export default function Weather(){
+  return (
+    <div className="Weather">
+      <form>
         <div className="row">
-          <div className="col-6">
-            <input
-              type="search"
-              className="form-control "
-              placeholder="Type a city..."
-            />
-          </div>
-          <div className="col-3">
-            <button type="submit" className="search-button  w=100">
-              Search
-            </button>
-          </div>
-  
-          <div className="col-3">
-            <button className="current-button w=100">Current</button>
-          </div>
+          <div className="col-9">
+        <input
+        type="search"
+        placeholder="Enter a city..."
+        className="form-control"
+        autoFocus="on"
+        />
         </div>
-        <div className="row">
-          <div className="col">
-            <div className="city-details">
-              <h1>{weatherData.city}</h1>
-              <h2>{weatherData.date}</h2>
-            </div>
-          </div>
-          <div className="col">
-            <div className="weather-icon-container">
-              <span className="degree">{weatherData.temperature}</span>
-              <span className="celsiusFahrenheit">
-                <a href="/" className="active">
-                  °C
-                </a>{" "}
-                | <a href="/">°F</a>
-              </span>
-              <br />
-              <img
-                src={weatherData.imgUrl}
-                alt={weatherData.description}
-                className="weatherImage"
-              ></img>
-            </div>
-          </div>
-          <div className="col">
-            <div className="city-weather">
-              <h3>{weatherData.description}</h3>
-              <ul className="city-stats">
-                <li className="humidity">
-                  Humidity: <span>{weatherData.humidity}</span>%
-                </li>
-                <li className="wind">
-                  Wind: <span>{weatherData.wind}</span>km/h
-                </li>
-              </ul>
-            </div>
-          </div>
+        <div className="col-3">
+        <input 
+        type="submit"
+        value="search"
+        className="btn btn-outline-secondary w-100"
+        />
         </div>
-        <div className="row">
-          <div className="col">
-            <div className="day-of-the-week">
-              <div>07:00 </div>
-              <div className="forecast-image">
-                <img 
-                src={weatherData.imgUrl} alt={weatherData.description}></img>
-              </div>
-              <div className="temp">
-                <span className="temp">{weatherData.temperature} </span>
-              </div>
-            </div>
-</div>
-
-
         </div>
-        
-        
-        <img src="/img/catBanner.jpg" alt="Cat" className="catBanner" />
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Mostly Cloudy" />
+       <span className="temperature">6</span>
+       <span className="unit"> °C</span>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 72%</li>
+            <li>Wind: 13 km/h</li>
+          </ul>
+        </div>
       </div>
-      <footer> <a href="https://github.com/ChiaCesa/react-weather-app" >Open-source code</a> by Chiara Cesana</footer>
-      </div>
-    );
-  }
+    </div>
+  )
+}
